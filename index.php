a1670f00a4af19cc0c3ab0c2a6dc58c28a515e4d
it63070064 <73683238+it63070064@users.noreply.github.com>
2020-10-30 12:49:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<?php
-echo "<h2>PHP in KMITL!</h2>";
-echo "Hello IT Smart!<br>";
-?>
+<html>
+    <head>
+        <meta>
+        <title>TRISIT PRANGKAEW 63070064 Jam</title>
+    </head>
+    <body>
+        <h1>TRISIT PRANGKAEW 63070064 Jam</h1>
+        <h1>Cat love Dog OK OK</h1>
+        <img src="chivava.jpg">
+        <img src="che.jpg">
+        <br>
+        <h3>Hello my name is Chivava</h3>
+        <h3>Nice to meet you everyone Love</h3>
+        <h3>What is a useless thing in ur life??</h3>
+        <h3>Prayut Chan-o-Cha III</h3>
+        <br>
+        <a href="http://www.it.kmitl.ac.th">"Click this Link!!"</a>
+        
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+    </body>
+</html>
